e994018b6b61ace38006ce460fb33f29702546b3
spaceC00kie <kirstie317@gmail.com>
2021-02-15 17:59:21 -0500

    I was right!

---
+++ props-test/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Franklin from './components/Franklin';
+
+function App() {
+  return (
+    <>
+      <Franklin command="sit" />
+      <Franklin command="stay" />
+      <Franklin command="up" />
+
+    </>
+  );
+}
+
+export default App;
---
+++ props-test/src/components/Franklin.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Franklin = ({command}) => {
+    return (
+        <>
+         {command}   
+        </>
+    )
+}
+
+export default Franklin
